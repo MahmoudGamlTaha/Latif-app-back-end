d7f417a298b5070dee40a973d8e024fe553ee7f4
Mahmoud <mahmoud.25490@gmail.com>
2021-03-27 18:04:28 +0200

    fix

--- src/main/java/com/commerce/backend/model/response/categoryType/CategoryTypeResponse.java
+++ src/main/java/com/commerce/backend/model/response/categoryType/CategoryTypeResponse.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class CategoryTypeResponse {
  private Long id;
- private boolean active;
+ private Boolean active;
  private String name;
  private String nameAr;
  private Date createdDate;
