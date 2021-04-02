11461337d8a042bcec95ed85b18a8c1e33177dab
Mahmoud Gamal Taha <mahmoud.25490@gmail.com>
2021-04-02 20:11:57 +0200

    Update CategoryController.java

--- src/main/java/com/commerce/backend/api/CategoryController.java
+++ src/main/java/com/commerce/backend/api/CategoryController.java
@@ -28,10 +28,9 @@ public class CategoryController extends PublicApiController {
         this.itemObjectCategoryService = itemObjectCategoryService;
     }
 
-    @GetMapping(value = "/cat-by-type")
-    @ResponseBody
-    public ResponseEntity<BasicResponse> getCategoryByAdsType(@RequestParam(value="id", required= true) Integer id){
-    	BasicResponse response = this.itemObjectCategoryService.findAllByTypeId(id);
+    @GetMapping(value = "/cat-by-type/{id}")
+    public ResponseEntity<BasicResponse> getCategoryByAdsType(@PathVariable(value="adtypeId", required= true) Integer adtypeId){
+    	BasicResponse response = this.itemObjectCategoryService.findAllByTypeId(adtypeId);
     	return new ResponseEntity<BasicResponse>(response, HttpStatus.OK);
     }
     @GetMapping(value = "/pet-category")
