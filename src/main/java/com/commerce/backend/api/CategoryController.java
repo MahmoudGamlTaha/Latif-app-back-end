ac11ce7a01509a57326a0f2c23a4ac917a06b3f7
Mahmoud Gamal Taha <mahmoud.25490@gmail.com>
2021-04-02 20:15:06 +0200

    Update CategoryController.java

--- src/main/java/com/commerce/backend/api/CategoryController.java
+++ src/main/java/com/commerce/backend/api/CategoryController.java
@@ -8,10 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,7 +27,7 @@ public class CategoryController extends PublicApiController {
         this.itemObjectCategoryService = itemObjectCategoryService;
     }
 
-    @GetMapping(value = "/cat-by-type/{id}")
+    @GetMapping(value = "/cat-by-type/{adtypeId}")
     public ResponseEntity<BasicResponse> getCategoryByAdsType(@PathVariable(value="adtypeId", required= true) Integer adtypeId){
     	BasicResponse response = this.itemObjectCategoryService.findAllByTypeId(adtypeId);
     	return new ResponseEntity<BasicResponse>(response, HttpStatus.OK);
