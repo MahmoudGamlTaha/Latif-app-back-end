97eefa72c70859b0b5b1ba0de5f98023b4ded3f7
Mahmoud <mahmoud.25490@gmail.com>
2021-05-04 02:53:23 +0200

    fix users ads

--- src/main/java/com/commerce/backend/model/entity/UserAccAds.java
+++ src/main/java/com/commerce/backend/model/entity/UserAccAds.java
@@ -5,6 +5,7 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.commerce.backend.constants.AdsType;
 
