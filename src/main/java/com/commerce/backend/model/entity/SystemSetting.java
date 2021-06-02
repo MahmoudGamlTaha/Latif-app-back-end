fd6d80dbc541edb49cae65efec60f6c9afdd5fe1
Mahmoud <mahmoud.25490@gmail.com>
2021-06-02 22:40:47 +0200

    systemSetting init

---
+++ src/main/java/com/commerce/backend/model/entity/SystemSetting.java
@@ -0,0 +1,13 @@
+package com.commerce.backend.model.entity;
+
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name = "system_setting")
+@Setter
+@Getter
+public class SystemSetting {
+
+}
